6af3919c1c71c369237afc33ef2155463f42fb17
cngsc <christinengsc97@gmail.com>
2023-09-04 13:45:46 -0700

    updatedfiles

---
+++ DEMO/setup.py
@@ -0,0 +1,31 @@
+import subprocess
+import sys
+
+# List of required dependencies
+dependencies = [
+    'csv',
+    'time',
+    'pandas',
+    'scipy',
+    'scikit-image',
+    'imageio',
+    'numpy',
+    'matplotlib',
+    'matplotlib.mlab',
+    'nd2',
+    'warnings',
+]
+
+def install_dependencies():
+    for package in dependencies:
+        try:
+            # Check if the package is already installed
+            __import__(package)
+        except ImportError:
+            print(f"Installing {package}...")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+if __name__ == "__main__":
+    print("Checking and installing dependencies...")
+    install_dependencies()
+    print("Dependencies installed successfully.")
